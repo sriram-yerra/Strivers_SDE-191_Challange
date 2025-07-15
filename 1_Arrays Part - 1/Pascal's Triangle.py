3a9a227053eb2ad1220da74b6d9ff0ca8053b094
Sriram <sriram.yerra@itspe.co.in>
2025-07-15 16:48:39 +0530

    pas tri

---
+++ 1_Arrays Part - 1/Pascal's Triangle.py
@@ -0,0 +1,35 @@
+from typing import List
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = []
+        for i in range(numRows):
+            res.append([])
+
+        for i in range(0, numRows):
+            res[i] = [0]*(i+1)
+
+        res[0][0] = 1
+
+        for i in range(1, numRows):
+            k = len(res[i])
+            res[i][0] = 1
+            res[i][k-1] = 1
+
+        for i in range(2, numRows):
+            p = res[i-1]
+            for j in range(len(res[i])):
+                if res[i][j] == 0:
+                    res[i][j] = res[i-1][j-1] + res[i-1][j]
+
+
+        return res
+        
+
+if __name__ == "__main__":
+    sol = Solution()
+    numRows = 1
+
+    res = sol.generate(numRows)
+
+    for row in res:
+        print(row)    
